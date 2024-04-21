c5e6f33377e18a95f07056f8f81631d3f28b5ca1
abeni-al7 <abenezeralebachew3@gmail.com>
2024-04-21 22:03:29 +0300

    States

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module starts a flask web app"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """Closes the storage"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Returns a list of all states"""
+    states = storage.all('State')
+    return render_template('7-states_list')
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
