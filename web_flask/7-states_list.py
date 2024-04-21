7d34c49cd93d241cfffb000c165d2346d1095523
abeni-al7 <abenezeralebachew3@gmail.com>
2024-04-21 22:14:11 +0300

    States

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,7 +17,7 @@ def states_list():
     """Returns a list of all states"""
     states = storage.all('State')
     return render_template('7-states_list')
-    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
